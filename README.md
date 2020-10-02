e9bac5fc0e870f24458fd6a9e7e6a883ba074477
Andrea_Graziosi <andreaggraziosi@gmail.com>
2020-10-01 21:52:22 -0700

    added four functions

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PoetrySlam
 CS1.0 Poetry Slam assignment
+In this assignmnet we are creating functions to manipulate hoe the lines of a poem are printed
+We have created 4 functions to open the txt file, print the poem lines backwards, print the lines randomly and a custom method of our choice.
--- poetry_slam.py
+++ poetry_slam.py
@@ -1,46 +1,60 @@
-import random
+from random import choice
 
+#worked with Joi , Sawywer
 def get_file_lines(filename):
     """ 
     return a list of strings containing the lines of the file.
     """
+    infile = open(filename, "r")
+    poem = infile.readlines()
+    infile.close()
+    return poem
 
-poem = "PoetrySlam/poem.txt"
-infile = open(poem, "r")
-        #   return infile.read()
+#print (get_file_lines('PoetrySlam/poem.txt'))
 
-# print(infile.read())
-infile.close()
-
+ 
 
 def lines_printed_backwards(lines_list):
-
+#worked with Joi , Sawywer
     """
    returns the lines of the poem in reverse
-
     """
-    f = open(lines_list, "r") #lines_list is a list of strings containing lines of my poem f is the opened poem in read mode
-    line_list = f.read().splitlines() # n/ effect so each line has a break 
-    line_num =len(line_list) #adds the line numbers to the length of line_list
-    line_list = line_list[::-1] #reversing
+    lines_list.reverse() #this reverses a list .reverse()
+    lines_list_length = len(lines_list)
    
-    for i in range(len(line_list)): 
-        reverse = str(line_num -i) + " " + line_list[i]
-        print(reverse)
-        infile.close()
-lines_printed_backwards("PoetrySlam/poem.txt")
+    for i in range(lines_list_length): #changed the for loops to use index so we can have the nums
+        current_line = lines_list_length[i]
+        line_num = lines_list_length - i
+        print(f"{(line_num} {current_line}")
+        
+
 
 def lines_printed_random(lines_list):
     """
     prints lines of poem in random order
     """ 
-    f = open(lines_list, "r") #lines_list is a list of strings containing lines of my poem f is the opened poem in read mode
-    line_list = f.read().splitlines() # n/ effect so each line has a break 
-    line_num =len(line_list) #adds the line numbers to the length of line_list
-
-    print(random.randint(1,32))#uses intergers not floats both numbers are inclusive
+    for line in lines_list:
+         print(choice(lines_list))
 
 
-def lines_printed_custom(Lines_list):
+def lines_printed_custom(lines_list):
 
-   
+    
+  vowels= ["a", "e", "i", "o", "u"]
+
+  #this loop will not print any lines sarting with a vowel
+  for line in lines_list:
+    # if it doesn't start with a vowel, print
+    if line[0] not in vowels:
+      print(line)
+    
+     
+        
+
+
+       
+poem_lines = get_file_lines("PoetrySlam/poem.txt")
+
+lines_printed_backwards(poem_lines)
+lines_printed_random(poem_lines)
+lines_printed_custom(poem_lines)
